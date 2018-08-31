6e336c1060d3225ea320405946834da3f2564e1b
Travis CI <travis@travis-ci.org>
2018-08-31 14:56:15 +0200

    Updated

--- push.sh
+++ push.sh
@@ -5,6 +5,5 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add .
 git commit -a -m "Updated"
-git remote add origin-pages https://${GH_TOKEN}@github.com/tomasbjerre/Tomas_Bjerre_AB.git
-git remote -v
+git remote add origin-pages https://$GH_TOKEN@github.com/tomasbjerre/Tomas_Bjerre_AB.git
 git push -u origin-pages master
